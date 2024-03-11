1ebfe16ff9d4c6d50d8b0a684a124fcf98542769
Mayank Mahajan <mayankmahajan.x@nxp.com>
2024-03-11 12:48:36 +0530

    making changes

--- drivers/sensor/vl53l4cd/vl53l4cd.c
+++ drivers/sensor/vl53l4cd/vl53l4cd.c
@@ -14,21 +14,33 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/i2c.h>
 
-#include "VL53L4CD_ULD/Platform/platform.h"
-#include "VL53L4CD_ULD/VL53L4CD_ULD_Driver/VL53L4CD_api.h"
+#include "vl53l4cd_api.h"
+#include "vl53l4cd_platform.h"
+
+LOG_MODULE_REGISTER(VL53L4CD, CONFIG_SENSOR_LOG_LEVEL);
+
+#define VL53L4CD_INITIAL_ADDR			0x29
 
 struct vl53l4cd_data {
 	struct k_sem lock;
+	VL53L4CD_Dev_t vl53l4cd;
+	// VL53L4CD_RangingMeasurementData_t RangingMeasurementData;
 };
 
 struct vl53l4cd_config {
 	const struct i2c_dt_spec i2c;
+	// struct gpio_dt_spec xshut;
 };
 
 static int vl53l4cd_init(const struct device *dev)
 {
+	struct vl53l4cd_data *data = dev->data;
 	const struct vl53l4cd_config *config = dev->config;
 
+	data->vl53l4cd.i2c_bus = config->i2c.bus;
+	data->vl53l4cd.i2c_dev_addr = VL53L4CD_INITIAL_ADDR;
+
+{ // testing.
 	uint8_t reg[2] = {0x01, 0x0F};
 	i2c_write(config->i2c.bus, reg, 2, 0x29);
 	uint8_t buf;
@@ -36,35 +48,36 @@ static int vl53l4cd_init(const struct device *dev)
 	printk("%x\n", buf);
 
 	uint8_t buf_2 = 0;
-	VL53L4CD_RdByte(config->i2c.bus, 0x0110, &buf_2);
+	VL53L4CD_RdByte(&(data->vl53l4cd), 0x0110, &buf_2);
 	printk("%x\n", buf_2);
 	uint16_t buf_3 = 0;
-	VL53L4CD_RdWord(config->i2c.bus, 0x0110, &buf_3);
+	VL53L4CD_RdWord(&(data->vl53l4cd), 0x0110, &buf_3);
 	printk("%x\n", buf_3);
 	uint32_t buf_4 = 0;
-	VL53L4CD_RdDWord(config->i2c.bus, 0x010F, &buf_4);
+	VL53L4CD_RdDWord(&(data->vl53l4cd), 0x010F, &buf_4);
 	printk("%x\n", buf_4);
 
-	const struct device *i2c = config->i2c.bus;
-
 	uint16_t id;
-	VL53L4CD_GetSensorId(i2c, &id);
+	VL53L4CD_GetSensorId(&(data->vl53l4cd), &id);
 	printk("%x\n", id);
 
-	id = 0x9999;
-	VL53L4CD_CheckForDataReady(i2c, &id);
-	printk("%x\n", id);
+	uint8_t id_2;
+	VL53L4CD_CheckForDataReady(&(data->vl53l4cd), &id_2);
+	printk("%x\n", id_2);
 
-	VL53L4CD_SensorInit(i2c);
+	VL53L4CD_SensorInit(&(data->vl53l4cd));
 
-	VL53L4CD_StartRanging(i2c);
+	VL53L4CD_StartRanging(&(data->vl53l4cd));
 
 	VL53L4CD_ResultsData_t p_result;
 	while (true) {
-		VL53L4CD_GetResult(i2c, &p_result);
+		VL53L4CD_GetResult(&(data->vl53l4cd), &p_result);
 		printk("p_result->distance_mm = %d\n", p_result.distance_mm);
+		printk("p_result->range_status = %d\n", p_result.range_status);
+		printk("p_result->signal_rate_kcps = %d\n", p_result.signal_rate_kcps);
 		k_sleep(K_MSEC(250));
 	}
+}
 
 	return 0;
 }
---
+++ drivers/sensor/vl53l4cd/vl53l4cd_platform.c
@@ -0,0 +1,162 @@
+/*
+ Copyright (c) 2021, STMicroelectronics - All Rights Reserved
+
+ This file : part of VL53L4CD Ultra Lite Driver and : dual licensed, either
+ 'STMicroelectronics Proprietary license'
+ or 'BSD 3-clause "New" or "Revised" License' , at your option.
+
+*******************************************************************************
+
+ 'STMicroelectronics Proprietary license'
+
+*******************************************************************************
+
+ License terms: STMicroelectronics Proprietary in accordance with licensing
+ terms at www.st.com/sla0081
+
+ STMicroelectronics confidential
+ Reproduction and Communication of this document : strictly prohibited unless
+ specifically authorized in writing by STMicroelectronics.
+
+
+*******************************************************************************
+
+ Alternatively, VL53L4CD Ultra Lite Driver may be distributed under the terms of
+ 'BSD 3-clause "New" or "Revised" License', in which case the following
+ provisions apply instead of the ones mentioned above :
+
+*******************************************************************************
+
+ License terms: BSD 3-clause "New" or "Revised" License.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ 3. Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software
+ without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************
+*/
+
+#include "vl53l4cd_platform.h"
+#include <zephyr/sys/byteorder.h>
+
+uint8_t VL53L4CD_RdDWord(VL53L4CD_Dev_t *dev, uint16_t RegisterAdress, uint32_t *value)
+{
+	uint8_t status = 255;
+
+	int ret;
+
+	RegisterAdress = BSWAP_16(RegisterAdress);
+	ret = i2c_write_read(dev->i2c_bus, dev->i2c_dev_addr, &RegisterAdress, 2, (uint8_t *) value, 4);
+	if (ret < 0) {
+		printk("i2c_write_read failed (%d)\n", ret);
+	}
+	*value = BSWAP_32(*value);
+
+	return status;
+}
+
+uint8_t VL53L4CD_RdWord(VL53L4CD_Dev_t *dev, uint16_t RegisterAdress, uint16_t *value)
+{
+	uint8_t status = 255;
+
+	int ret;
+
+	RegisterAdress = BSWAP_16(RegisterAdress);
+	ret = i2c_write_read(dev->i2c_bus, dev->i2c_dev_addr, &RegisterAdress, 2, (uint8_t *) value, 2);
+	if (ret < 0) {
+		printk("i2c_write_read failed (%d)\n", ret);
+	}
+	*value = BSWAP_16(*value);
+
+	return status;
+}
+
+uint8_t VL53L4CD_RdByte(VL53L4CD_Dev_t *dev, uint16_t RegisterAdress, uint8_t *value)
+{
+	uint8_t status = 255;
+
+	int ret;
+
+	RegisterAdress = BSWAP_16(RegisterAdress);
+	ret = i2c_write_read(dev->i2c_bus, dev->i2c_dev_addr, &RegisterAdress, 2, value, 1);
+	if (ret < 0) {
+		printk("i2c_write_read failed (%d)\n", ret);
+	}
+
+	return status;
+}
+
+uint8_t VL53L4CD_WrByte(VL53L4CD_Dev_t *dev, uint16_t RegisterAdress, uint8_t value)
+{
+	uint8_t status = 255;
+
+	int ret;
+
+	RegisterAdress = BSWAP_16(RegisterAdress);
+	uint8_t buf[3];
+	memcpy(buf, &RegisterAdress, 2);
+	memcpy(buf + 2, &value, 1);
+	ret = i2c_write(dev->i2c_bus, buf, 3, dev->i2c_dev_addr);
+
+	return status;
+}
+
+uint8_t VL53L4CD_WrWord(VL53L4CD_Dev_t *dev, uint16_t RegisterAdress, uint16_t value)
+{
+	uint8_t status = 255;
+
+	int ret;
+
+	value = BSWAP_16(value);
+	RegisterAdress = BSWAP_16(RegisterAdress);
+	uint8_t buf[4];
+	memcpy(buf, &RegisterAdress, 2);
+	memcpy(buf + 2, &value, 2);
+	ret = i2c_write(dev->i2c_bus, buf, 4, dev->i2c_dev_addr);
+
+	return status;
+}
+
+uint8_t VL53L4CD_WrDWord(VL53L4CD_Dev_t *dev, uint16_t RegisterAdress, uint32_t value)
+{
+	uint8_t status = 255;
+
+	int ret;
+
+	value = BSWAP_32(value);
+	RegisterAdress = BSWAP_16(RegisterAdress);
+	uint8_t buf[6];
+	memcpy(buf, &RegisterAdress, 2);
+	memcpy(buf + 2, &value, 4);
+	ret = i2c_write(dev->i2c_bus, buf, 6, dev->i2c_dev_addr);
+
+	return status;
+}
+
+uint8_t WaitMs(VL53L4CD_Dev_t *dev, uint32_t TimeMs)
+{
+	uint8_t status = 255;
+	/* To be filled by customer */
+	k_sleep(K_MSEC(TimeMs));
+	return status;
+}

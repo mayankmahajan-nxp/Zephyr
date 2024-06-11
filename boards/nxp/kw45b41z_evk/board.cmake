13556d775a0fef0e1f088f8762f7bd5b798c16cd
Mayank Mahajan <mayankmahajan.x@nxp.com>
2024-06-11 15:54:37 +0530

    Renaming files.
    Signed-off-by: Mayank Mahajan <mayankmahajan.x@nxp.com>

--- boards/nxp/kw45b41z_evk/board.cmake
+++ boards/nxp/kw45b41z_evk/board.cmake
@@ -1,6 +1,6 @@
 # Copyright 2024 NXP
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=kw45b41z83" "--reset-after-load")
+# board_runner_args(jlink "--device=KW45B41Z83AFTA" "--reset-after-load")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)

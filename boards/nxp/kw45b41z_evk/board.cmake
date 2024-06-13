b3ad3309c84d97f5ba63b578498a78c848fb6227
Mayank Mahajan <mayankmahajan.x@nxp.com>
2024-06-13 10:15:12 +0530

    Building but need to resolve warning and flashing problem.
    Signed-off-by: Mayank Mahajan <mayankmahajan.x@nxp.com>

--- boards/nxp/kw45b41z_evk/board.cmake
+++ boards/nxp/kw45b41z_evk/board.cmake
@@ -2,6 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # board_runner_args(jlink "--device=KW45B41Z83AFTA" "--reset-after-load")
-# board_runner_args(jlink "--device=kw45b41z83" "--reset-after-load")
+board_runner_args(jlink "--device=kw45b41z83" "--reset-after-load")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
